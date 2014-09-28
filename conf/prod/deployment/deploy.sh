96e7946ee66aadaed20d81b19337f6aa7822f7b9
Ivan Skrypka <vanish.com90@gmail.com>
2014-09-28 21:03:16 +0300

    deployment script

---
+++ conf/prod/deployment/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+APPLICATION_SERVER_IP=$1
+chmod 400 conf/prod/deployment/eventstech-key.pem
+
+ssh -i conf/prod/deployment//eventstech-key.pem ubuntu@${APPLICATION_SERVER_IP} 'bash -s' <<'ENDSSH'
+    bash /home/ubuntu/tomcat/bin/shutdown.sh
+    rm -r /home/ubuntu/tomcat/webapps/eventstech
+    rm -r /home/ubuntu/tomcat/work/localhost/eventstech
+    rm /home/ubuntu/tomcat/conf/context.xml
+    rm -r /home/ubuntu/tomcat/logs/*
+    rm /home/ubuntu/tomcat/webapps/eventstech.war
+    rm -r /home/ubuntu/conf/*
+ENDSSH
+
+scp -r -i conf/prod/deployment/eventstech-key.pem ubuntu@${APPLICATION_SERVER_IP} conf/prod/properties/* ubuntu@${APPLICATION_SERVER_IP}:/home/ubuntu/conf
+scp -r -i conf/prod/deployment/eventstech-key.pem ubuntu@${APPLICATION_SERVER_IP} conf/prod/tomcat/context.xml ubuntu@${APPLICATION_SERVER_IP}:/home/ubuntu/tomcat/conf
+scp -r -i conf/prod/deployment/eventstech-key.pem ubuntu@${APPLICATION_SERVER_IP} rest-api-aggregator/build/libs/eventstech.war ubuntu@${APPLICATION_SERVER_IP}:/home/ubuntu/tomcat/webapps
+
+ssh -i conf/prod/deployment//eventstech-key.pem ubuntu@${APPLICATION_SERVER_IP} 'bash -s' <<'ENDSSH'
+    bash /home/ubuntu/tomcat/bin/startup.sh
+ENDSSH
+
+chmod 666 conf/prod/deployment/eventstech-key.pem
