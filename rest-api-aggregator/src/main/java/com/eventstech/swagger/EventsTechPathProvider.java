8e561e63e2770e15e44412649fbb65be583f5dbf
skrypkaivan <ivan.skrypka@statiq.co>
2014-10-01 16:21:48 +0300

    api documentation plugin

---
+++ rest-api-aggregator/src/main/java/com/eventstech/swagger/EventsTechPathProvider.java
@@ -0,0 +1,15 @@
+package com.eventstech.swagger;
+
+import com.mangofactory.swagger.core.DefaultSwaggerPathProvider;
+
+/**
+ * Date: 01.10.14
+ * Author: Ivan Skrypka
+ * Copyright © 2014 Statiq, Inc.
+ */
+public class EventsTechPathProvider extends DefaultSwaggerPathProvider {
+    @Override
+    public String getAppBasePath() {
+        return "/eventstech/app";
+    }
+}
