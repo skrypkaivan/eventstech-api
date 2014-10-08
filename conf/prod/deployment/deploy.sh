b0016115bfbc4d53a8e524a9b3ce16bdff584016
skrypkaivan <ivan.skrypka@statiq.co>
2014-10-08 17:14:27 +0300

    deployment script changes

--- conf/prod/deployment/deploy.sh
+++ conf/prod/deployment/deploy.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-APPLICATION_SERVER_IP=$1
 chmod 400 conf/prod/deployment/eventstech-key.key
 
-ssh -i conf/prod/deployment/eventstech-key.key ubuntu@${APPLICATION_SERVER_IP} 'bash -s' <<'ENDSSH'
-    bash /home/ubuntu/tomcat/bin/shutdown.sh
-    rm -rf /home/ubuntu/tomcat/webapps/eventstech
-    rm -rf /home/ubuntu/tomcat/work/localhost/eventstech
-    rm /home/ubuntu/tomcat/conf/context.xml
-    rm -r /home/ubuntu/tomcat/logs/*
-    rm -f /home/ubuntu/tomcat/webapps/eventstech.war
-    rm -r /home/ubuntu/conf/*
-    rm -rf /home/ubuntu/tools
-    mkdir /home/ubuntu/tools
+ssh -i conf/prod/deployment/eventstech-key.key ${SYSTEM_USERNAME}@${APPLICATION_SERVER_IP} 'bash -s' <<'ENDSSH'
+    bash ${APPLICATION_HOME}/tomcat/bin/shutdown.sh
+    rm -rf ${APPLICATION_HOME}/tomcat/webapps/eventstech
+    rm -rf ${APPLICATION_HOME}/tomcat/work/localhost/eventstech
+    rm ${APPLICATION_HOME}/tomcat/conf/context.xml
+    rm -r ${APPLICATION_HOME}/tomcat/logs/*
+    rm -f ${APPLICATION_HOME}/tomcat/webapps/eventstech.war
+    rm -r ${APPLICATION_HOME}/conf/*
+    rm -rf ${APPLICATION_HOME}/tools
+    mkdir ${APPLICATION_HOME}/tools
 ENDSSH
 
-scp -r -i conf/prod/deployment/eventstech-key.pem conf/prod/properties/* ubuntu@${APPLICATION_SERVER_IP}:/home/ubuntu/conf
-scp -r -i conf/prod/deployment/eventstech-key.pem conf/prod/tomcat/context.xml ubuntu@${APPLICATION_SERVER_IP}:/home/ubuntu/tomcat/conf
-scp -r -i conf/prod/deployment/eventstech-key.pem tools/build/distributions/eventstech-tools.zip ubuntu@${APPLICATION_SERVER_IP}:/home/ubuntu/tools
-scp -r -i conf/prod/deployment/eventstech-key.pem rest-api-aggregator/build/libs/eventstech.war ubuntu@${APPLICATION_SERVER_IP}:/home/ubuntu/tomcat/webapps
+scp -r -i conf/prod/deployment/eventstech-key.pem conf/prod/properties/* ${SYSTEM_USERNAME}@${APPLICATION_SERVER_IP}:${APPLICATION_HOME}/conf
+scp -r -i conf/prod/deployment/eventstech-key.pem conf/prod/tomcat/context.xml ${SYSTEM_USERNAME}@${APPLICATION_SERVER_IP}:${APPLICATION_HOME}/tomcat/conf
+scp -r -i conf/prod/deployment/eventstech-key.pem tools/build/distributions/eventstech-tools.zip ${SYSTEM_USERNAME}@${APPLICATION_SERVER_IP}:${APPLICATION_HOME}/tools
+scp -r -i conf/prod/deployment/eventstech-key.pem rest-api-aggregator/build/libs/eventstech.war ${SYSTEM_USERNAME}@${APPLICATION_SERVER_IP}:${APPLICATION_HOME}/tomcat/webapps
 
-ssh -i conf/prod/deployment/eventstech-key.key ubuntu@${APPLICATION_SERVER_IP} 'bash -s' <<'ENDSSH'
-    unzip /home/ubuntu/tools/eventstech-tools.zip
-    rm /home/ubuntu/tooks/eventstech-tools.zip
-    bash /home/ubuntu/tomcat/bin/startup.sh
+ssh -i conf/prod/deployment/eventstech-key.key ${SYSTEM_USERNAME}@${APPLICATION_SERVER_IP} 'bash -s' <<'ENDSSH'
+    unzip ${APPLICATION_HOME}/tools/eventstech-tools.zip
+    rm ${APPLICATION_HOME}/tooks/eventstech-tools.zip
+    bash ${APPLICATION_HOME}/tomcat/bin/startup.sh
 ENDSSH
 
 chmod 666 conf/prod/deployment/eventstech-key.key
